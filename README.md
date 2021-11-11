cd46d3588980b18c1baef33cf46d89e6591f9bce
edel-ops <ebonilla6@gmail.com>
2021-11-10 22:07:07 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-simple-calc-form-example-djw4xu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-simple-calc-form-example-djw4xu)
---
+++ index.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+function FormularioSuma() {
+  const [sum, setSum] = useState(0);
+  const [num, setNum] = useState(0);
+
+  function manejadorCambio(x) {
+    setNum(x.target.value);
+  }
+
+  function manejadorSubmit(y) {
+    setSum(sum + Number(num));
+    y.preventDefault();
+  }
+
+  return <form onSubmit={manejadorSubmit}>
+  <input type="number" value={num} onChange={manejadorCambio} />
+  <input type="submit" value="Add" />
+  <p> Sum is {sum} </p>
+  </form>;
+}
+
+const el = <FormularioSuma />; 
+ReactDOM.render(
+  el, 
+  document.getElementById('root')
+);
